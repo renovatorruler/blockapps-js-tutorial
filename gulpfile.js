b630397424454dbf6b03155ff50f977e95e55438
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-12-13 13:36:36 -0500

    -Added dist to gitignore -Working browsersync livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,9 @@
+var paths = {
+ scripts: ['app/scripts/**/*.js'],
+ html: ['app/index.html', '!app/test.html'], 
+ dist: 'dist'
+};
+
 var gulp = require("gulp");
 var browserSync = require('browser-sync').create();
 
@@ -6,21 +12,29 @@ var browserSync = require('browser-sync').create();
 var babel = require("gulp-babel");
 
 gulp.task("babel", function () {
-  return gulp.src("app/scripts/app.es6.js")
-    .pipe(babel());
+  return gulp.src(paths.scripts)
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task("dist", function () {
+  gulp.src(paths.html)
+    .pipe(gulp.dest(paths.dist));
 });
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['babel'], function() {
 
     browserSync.init({
-        server: "./app"
+        server: paths.dist
     });
 
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch(paths.scripts, ['babel']);
+    gulp.watch(paths.html, ['dist']).on('change', browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', ['serve']);
+gulp.task('default', ['dist', 'serve']);
 
