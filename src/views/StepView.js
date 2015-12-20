e1e1c92d0b6fbc00b8533b3a61cda5a19812f69f
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-12-20 03:15:38 -0500

    -eslint validation

--- src/views/StepView.js
+++ src/views/StepView.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export class StepView extends React.Component {
     render () {
-        return (
-            <p>This is the page 1 of the tutorial</p>
-        )
+      return (
+          <p>This is the page 1 of the tutorial</p>
+      )
     }
 }
 
--- src/views/TutorialView.js
+++ src/views/TutorialView.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export class TutorialView extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.element
+  }
   render () {
     return (
       <div className='container text-center'>
