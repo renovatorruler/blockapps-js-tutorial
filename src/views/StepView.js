86785c3eb5932658e959b4698a7a51e296d4de57
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-12-29 00:04:24 -0500

    -Added React-ace library -Working code editor in StepView

--- src/views/StepView.js
+++ src/views/StepView.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import AceEditor from 'react-ace'
+
+import 'brace/mode/javascript'
+import 'brace/theme/github'
 
 export class StepView extends React.Component {
     static propTypes = {
       params: React.PropTypes.object.isRequired
     }
+    onChange (newValue) {
+      console.log('changed', newValue)
+    }
     render () {
       return (
-          <p>This is the page { this.props.params.step } of the tutorial</p>
+          <div>
+            <AceEditor
+                mode='javascript'
+                theme='github'
+                height='400'
+                onChange={this.onChange}
+                name='Step{this.props.params.step}'
+            />
+            <p>This is the page { this.props.params.step } of the tutorial</p>
+          </div>
       )
     }
 }
