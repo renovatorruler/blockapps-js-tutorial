11e2b85eab0687c56d98891a1042222a9005a58a
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-01-03 21:52:25 -0500

    -Working readonly area feature

--- src/views/blockapps-intro/IncludeLibraryView.js
+++ src/views/blockapps-intro/IncludeLibraryView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AceEditor from 'react-ace'
 import Hightlight from 'react-highlight'
+import Ace from 'brace'
 
 import 'brace/mode/html'
 import 'brace/mode/javascript'
@@ -8,9 +9,13 @@ import 'brace/theme/github'
 
 import 'highlight.js/styles/default.css'
 
+const Anchor = Ace.acequire('ace/anchor').Anchor
+const Range = Ace.acequire('ace/range').Range
+
 export class IncludeLibraryView extends React.Component {
     static defaultProps = {
       codeBlock: `<script type="text/javascript" src="libs/blockapps-min.js"></script>`,
+      editableArea: [[2, 0, 3, 0]],
       template: `<html>
   <head>
     [ADD THE SCRIPT TAG HERE]
@@ -23,9 +28,12 @@ export class IncludeLibraryView extends React.Component {
     constructor (props) {
       super(props)
       this.state = {
+        editor: {},
+        anchors: [],
         userCode: this.props.template
       }
       this.handleProceed = this.handleProceed.bind(this)
+      this.handleKeyboard = this.handleKeyboard.bind(this)
       this.onEditorLoad = this.onEditorLoad.bind(this)
       this.onEditorChange = this.onEditorChange.bind(this)
     }
@@ -37,6 +45,7 @@ export class IncludeLibraryView extends React.Component {
     static propTypes = {
       step: React.PropTypes.number.isRequired,
       codeBlock: React.PropTypes.string,
+      editableArea: React.PropTypes.array,
       template: React.PropTypes.string
     }
 
@@ -45,9 +54,38 @@ export class IncludeLibraryView extends React.Component {
     }
 
     onEditorLoad (editor) {
+      this.state.editor = editor
+      this.state.doc = editor.getSession().getDocument()
+
       editor.renderer.setScrollMargin(10, 10, 10, 10)
       editor.getSession().setUseWorker(false)
       editor.moveCursorTo(2, 0)
+
+      let startAnchor, endAnchor
+      for (let area of this.props.editableArea) {
+        startAnchor = new Anchor(this.state.doc, area[0], area[1])
+        endAnchor = new Anchor(this.state.doc, area[2], area[3])
+        this.state.anchors.push([startAnchor, endAnchor])
+      }
+
+      editor.keyBinding.addKeyboardHandler({
+        handleKeyboard: this.handleKeyboard
+      })
+    }
+
+    handleKeyboard (data, hash, keyString, keyCode, event) {
+      if (hash === -1 || (keyCode <= 40 && keyCode >= 37)) return false
+
+      const cursorPosition = this.state.editor.getCursorPosition()
+      let insideReadOnly = false
+      let editableRange
+      for (let anchor of this.state.anchors) {
+        editableRange = Range.fromPoints(anchor[0].getPosition(), anchor[1].getPosition())
+        insideReadOnly = !editableRange.contains(cursorPosition.row, cursorPosition.column)
+      }
+      if (insideReadOnly) {
+        return {command: 'null', passEvent: false}
+      }
     }
 
     handleProceed () {
@@ -75,10 +113,10 @@ export class IncludeLibraryView extends React.Component {
                     mode='html'
                     theme='github'
                     onChange={this.onEditorChange}
-                    onEditorLoad={this.onEditorLoad}
+                    onLoad={this.onEditorLoad}
                     width='100%'
                     height='15em'
-                    name='editor'
+                    name='includeLibraryEditor'
                     editorProps={{$blockScrolling: true}}
                     value={this.props.template}
                 />
