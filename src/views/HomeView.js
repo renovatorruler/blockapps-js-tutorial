1531d51545a1ba237acd4623e94f4966088b3c1d
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-01-24 17:41:22 -0500

    Misc fixes
    -Fixed StepView error
    -Removed old Code from HomeView
    -Fixed Copy

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,27 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import { actions as counterActions } from '../redux/modules/counter'
 
-// We define mapStateToProps where we'd normally use
-// the @connect decorator so the data requirements are clear upfront, but then
-// export the decorated component after the main class definition so
-// the component can be tested w/ and w/o being connected.
-// See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
-const mapStateToProps = (state) => ({
-  counter: state.counter
-})
 export class HomeView extends React.Component {
-  static propTypes = {
-    counter: React.PropTypes.number.isRequired,
-    doubleAsync: React.PropTypes.func.isRequired,
-    increment: React.PropTypes.func.isRequired
-  }
 
   render () {
     return (
       <div className='container text-center'>
-        <h1>Welcome to the Tutorial</h1>
+        <h1>Welcome to Blockapps.js Tutorial</h1>
         <hr />
         <Link to='/tutorial/step/1'>Go To the Tutorial</Link>
       </div>
@@ -29,4 +15,4 @@ export class HomeView extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, counterActions)(HomeView)
+export default HomeView
