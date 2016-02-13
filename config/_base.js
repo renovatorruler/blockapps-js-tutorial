aecd5e873c933603d14abddff27ebc5cba9ec411
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-02-13 16:47:58 -0500

    Tying to fix the route issue

--- config/_base.js
+++ config/_base.js
@@ -31,7 +31,7 @@ const config = {
   compiler_hash_type       : 'hash',
   compiler_fail_on_warning : false,
   compiler_quiet           : false,
-  compiler_public_path     : './',
+  compiler_public_path     : './blockapps-js-tutorial/',
   compiler_stats           : {
     chunks : false,
     chunkModules : false,
