d1c3f5fd1bb123290a38133041e415d2217f9e08
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-02-13 21:55:52 -0500

    fixing the path

--- config/_production.js
+++ config/_production.js
@@ -3,7 +3,7 @@ export default (config) => ({
   compiler_fail_on_warning : false,
   compiler_hash_type       : 'chunkhash',
   compiler_source_maps     : false,
-  compiler_public_path     : 'http://prashantpawar.github.io/blockapps-js-tutorial/',
+  compiler_public_path     : './',
   compiler_stats           : {
     chunks : true,
     chunkModules : true,
